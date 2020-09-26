5401706d0a535ab651690050378f6edd7abfc432
YungHsiu <jeff1227alice@gmail.com>
2020-09-26 12:56:32 +0800

    modify postgresql connection config

--- knexfile.js
+++ knexfile.js
@@ -36,19 +36,20 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: __dirname +'/db/migrations'
+    },
   }
 
 };
